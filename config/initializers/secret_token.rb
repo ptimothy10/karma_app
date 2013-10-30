30e77b0f32325c0fd2213e20be4ad4d6c335756e
Henry Lilan <lilanhenry@gmail.com>
2013-10-30 18:15:13 +0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KarmaApp::Application.config.secret_key_base = '4a68cd106a08fd4392b6156a96043fa49d1b96fa7071e787af09f3534b5f1e8ca2d80eea7901fdc32992459e405156156992a520af1cdd3241a2b3b6723a6fdf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KarmaApp::Application.config.session_store :cookie_store, key: '_karma_app_session'
